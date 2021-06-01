6bcd5808b6b59427554a54a0cc4913966c8add6b
AlexanderJChoi <alexanderajchoi@gmail.com>
2021-06-01 14:56:43 -0700

    Add visutalization test directory

---
+++ testvisualization/small.py
@@ -0,0 +1,23 @@
+import snap
+
+Rnd = snap.TRnd()
+
+randgraph = snap.GenRndGnm(snap.TUNGraph, 100, 1000, False, Rnd)
+print("Rand Graph created")
+labels = {}
+for NI in randgraph.Nodes():
+    labels[NI.GetId()] = str(NI.GetId())
+print("Rand Graph labels created")
+randgraph.DrawGViz(snap.gvlSfdp, "randsmall.png", "Random graph on 100 nodes and 1000 edges", labels)
+print("Rand Graph Drawing complete")
+
+
+prefgraph = snap.GenPrefAttach(100, 10, Rnd)
+print("pref graph created")
+labels = {}
+for NI in prefgraph.Nodes():
+    labels[NI.GetId()] = str(NI.GetId())
+print("pref graph labels created")
+prefgraph.DrawGViz(snap.gvlSfdp, "prefsmall.png", "Preferentially attached graph on 100 nodes", labels)
+print("pref graph Drawing complete")
+
