84420cc29e3618b3283cbca59a766e25a9fa7e68
AlexanderJChoi <alexanderajchoi@gmail.com>
2021-06-02 19:24:33 -0700

    Fix method small_cluster_average_size() - now correctly computes the average (not including the largest cluster size) - or returns -1 if there are no clusters

--- g_metrics.py
+++ g_metrics.py
@@ -8,9 +8,16 @@ def largest_cluster_size(un_graph):
 
 def average_small_cluster_size(un_graph):
     total = 0
+    num_clusters = 0
     for comp in un_graph.GetSccSzCnt():
         total+=(comp.GetVal1() * comp.GetVal2())
-    return total - un_graph.GetMxScc().GetNodes()
+        num_clusters+=comp.GetVal2()
+    if num_clusters <= 0:
+        return -1
+    elif num_clusters == 1:
+        return total
+    else:
+        return (total - un_graph.GetMxScc().GetNodes()) / (num_clusters - 1)
 
 def global_eff(un_graph):
     return 0
